c9093e5676a667d7b1a9f1cf9fdb640a8d2f7973
Ang Li <ang6li98@gmail.com>
2022-07-14 14:08:45 -0700

    routine change

--- test_runtime.py
+++ test_runtime.py
@@ -7,7 +7,7 @@ import time
 left = cv2.imread("test_images/left.png", cv2.IMREAD_GRAYSCALE)
 right = cv2.imread("test_images/right.png", cv2.IMREAD_GRAYSCALE)
 
-scale_percent = 70 # percent of original size
+scale_percent = 100 # percent of original size
 width = int(left.shape[1] * scale_percent / 100)
 height = int(left.shape[0] * scale_percent / 100)
 dim = (width, height)
@@ -38,6 +38,3 @@ for i in range(100):
 print("Runtime for 100 calls of compute():", time.process_time() - start)
 
 depthEngine.close()
-
-plt.imshow(depth)
-plt.show()
